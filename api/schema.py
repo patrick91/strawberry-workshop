89af90e7ea034ef916c4c7f8c30aef472cb935f9
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-08 11:44:45 +0100

    Add tracing

--- api/schema.py
+++ api/schema.py
@@ -1,8 +1,10 @@
 import strawberry
+from strawberry.extensions import ValidationCache
 
 from .authentication.mutation import AuthenticationMutation
 from .podcasts.mutation import PodcastsMutation
 from .podcasts.query import PodcastsQuery
+from .tracing import DatadogTracingExtension
 
 
 @strawberry.type
@@ -15,4 +17,8 @@ class Mutation(AuthenticationMutation, PodcastsMutation):
     ...
 
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+    extensions=[DatadogTracingExtension, ValidationCache()],
+)
--- podcast/settings.py
+++ podcast/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "db",
     "users",
+    "ddtrace.contrib.django",
 ]
 
 MIDDLEWARE = [
