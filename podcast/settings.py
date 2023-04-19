fef8e68ece6fd59439994ea2bed1dc21fa6c5b9e
Patrick Arminio <patrick.arminio@gmail.com>
2023-04-19 08:22:14 -0600

    Use db from url if any

--- podcast/settings.py
+++ podcast/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +78,11 @@ WSGI_APPLICATION = "podcast.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
